099a7c148a7722d80e62bf53841538bf2009e6ee
Mini <kyaminik@gmail.com>
2024-07-20 23:01:11 +0530

    updated one

---
+++ src/ex_20072024/Loops082.java
@@ -0,0 +1,17 @@
+package ex_20072024;
+
+public class Loops082 {
+    public static void main(String[] args) {
+
+
+        //Condition and Loops
+        //
+        //age >18 then allowed to vote / else not eligible
+        int age = 66;
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not Eligible to vote");
+        }
+    }
+}
